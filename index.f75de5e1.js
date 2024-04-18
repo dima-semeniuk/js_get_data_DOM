3aefdc2147cf64bb0a4b1e2524b8381b93223548
Dima Semeniuk <semeniuk.dima@ukr.net>
2024-04-18 13:03:35 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const populations = document.querySelectorAll("span.population");
+const averagePopulationSpan = document.querySelector("span.average-population");
+const totalPopulationSpan = document.querySelector("span.total-population");
+const total = [
+    ...populations
+].reduce((sum, p)=>{
+    const val = +p.textContent.split(",").join("");
+    return sum + val;
+}, 0);
+const avarage = (total / populations.length).toLocaleString("en-US");
+averagePopulationSpan.textContent = avarage;
+totalPopulationSpan.textContent = total.toLocaleString("en-US");
+
+//# sourceMappingURL=index.f75de5e1.js.map
